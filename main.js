c8bd565d517ceb3cd72ea75eeadf927ae53d4a67
Jason Kwak <ralph9696@naver.com>
2022-08-27 13:03:33 -0400

    commit tree 9f04a3e428b03caf48466630aed04d64e2c9b211

--- main.js
+++ main.js
@@ -1,9 +1,43 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const error = document.querySelector('#modal')
 
 // Your JavaScript code goes here!
+error.classList.add("hidden")
+document.addEventListener('DOMContentLoaded', () => {
+  
+  
+  likes()
 
+})
+
+function likes() {
+  document.addEventListener('click', (event) => {
+    if(event.target.classList[0] === "like-glyph") {
+      mimicServerCall()
+        .then((res) => {
+          const activated = event.target.classList.contains("activated-heart")
+          if(activated) {
+            event.target.classList.remove("activated-heart")
+            event.target.innerHTML = EMPTY_HEART
+          } else{
+            event.target.classList.add("activated-heart")
+            event.target.innerHTML = FULL_HEART
+          }
+          
+          activated
+        })
+        .catch(err => {
+          error.classList.remove("hidden")
+          setTimeout(() => {
+            error.classList.add("hidden")
+          }, 3000)
+        })
+    }
+  })
+  
+}
 
 
 
